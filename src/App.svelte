4f01fcbe5a2e0f5bec107450d6e2f511f6e02fec
d.pavlov <dmitry.fromtolyatti@yandex.ru>
2021-04-22 15:12:36 +0400

    add svelte-materialify

--- src/App.svelte
+++ src/App.svelte
@@ -1,25 +1,31 @@
 <script lang="ts">
+	import { MaterialApp } from 'svelte-materialify';
+	import Button from 'svelte-materialify/src/components/Button/Button.svelte';
+
 	export let name: string;
 </script>
 
-<main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-</main>
+<MaterialApp>
+	<main>
+		<h1>Hello {name}!</h1>
+		<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+		<Button class="red white-text">Red</Button>
+	</main>
+</MaterialApp>
 
-<style>
+<style lang="scss">
 	main {
 		text-align: center;
 		padding: 1em;
 		max-width: 240px;
 		margin: 0 auto;
-	}
 
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
+		& > h1 {
+			color: #00bcd4;
+			text-transform: uppercase;
+			font-size: 4em;
+			font-weight: 600;
+		}
 	}
 
 	@media (min-width: 640px) {
